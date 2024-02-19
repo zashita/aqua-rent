e868aea65d83f19debba04a9f0bd54039248d5c7
ABARONA <98054848+zashita@users.noreply.github.com>
2024-02-20 00:24:25 +0300

    navbar authorization response

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -7,3 +7,5 @@ export {
     User,
     UserSchema,
 } from './model/types/user';
+
+export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -4,6 +4,8 @@ import { AppLink, AppLinkThemes } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonThemes } from 'shared/ui/Button/Button';
 import { LoginModal } from 'features/AuthByUsername';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserAuthData, userActions } from 'entities/User';
 import cls from './Navbar.module.scss';
 
 export interface NavbarProps{
@@ -11,10 +13,45 @@ export interface NavbarProps{
 }
 export const Navbar:React.FC<NavbarProps> = ({ className }) => {
     const { t } = useTranslation('translation');
+    const dispatch = useDispatch();
+    const authData = useSelector(getUserAuthData);
     const [authModal, setAuthModal] = useState(false);
     const toggleModal = useCallback(() => {
         setAuthModal((prevState) => !prevState);
     }, []);
+
+    const onLogout = () => {
+        dispatch(userActions.logout());
+    };
+
+    if (authData) {
+        return (
+            <div className={classNames(cls.Navbar, {}, [className])}>
+                <AppLink
+                    to="/"
+                    theme={AppLinkThemes.INVERTED}
+                >
+                    {t('Главная')}
+
+                </AppLink>
+                <AppLink
+                    to="/about"
+                    theme={AppLinkThemes.INVERTED}
+                >
+                    {t('О сайте')}
+                </AppLink>
+                <Button
+                    theme={ButtonThemes.CLEAR}
+                    className={cls.Button}
+                    onClick={onLogout}
+                >
+                    {t('Выйти')}
+                </Button>
+
+            </div>
+        );
+    }
+
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <AppLink
